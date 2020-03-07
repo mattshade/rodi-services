16597cbed1a0db39a2ce7df568e5add1f4075965
mattshade <matt.shade@nbcuni.com>
2020-03-07 14:27:11 -0500

    improves meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,15 @@
  */
 
 import React from "react"
+import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
 
 import { Container, Row, Col } from "react-bootstrap"
 
 import Navbar from "./navBar"
+
+import ogImage from '../images/rodi-icon.png'
+const siteOgImage = `https://www.rodiservice.com${ogImage}`
 
 const Layout = ({ children, pageInfo }) => (
   <StaticQuery
@@ -25,6 +29,24 @@ const Layout = ({ children, pageInfo }) => (
     `}
     render={data => (
       <>
+      <Helmet
+          title={`Rodi Services LLC`}
+          meta={[
+            { name: 'description', content: 'Residential Inspections, Pre-Listing Inspections, Commercial Inspections, Wood-Destroying Organisms/Termites/Pests Inspection, Mold Inspections/Assessments indoor air quality testing & More!' },
+            { name: 'keywords', content: 'home inspection, residential inspection,  commercial inspection, rodi, mark rodi' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content:'https://www.rodiservice.com' },
+            { property: 'og:site_name', content:'Rodi Services LLC' },
+            { property: 'og:title', content:'Rodi Services LLC' },
+            { property: 'og:description', content:'Residential Inspections, Pre-Listing Inspections, Commercial Inspections, Wood-Destroying Organisms/Termites/Pests Inspection, Mold Inspections/Assessments indoor air quality testing & More!' },
+            { property: 'og:image', content:`${siteOgImage}` },
+            { property: 'og:image:secure_url', content:`${siteOgImage}` },
+            { property: 'og:image:type', content:'image/png' },
+            { property: 'og:image:width', content:'512' },
+            { property: 'og:image:height', content:'512' },
+            { property: 'og:image:alt', content:'rodiservice.com' },
+          ]}
+        />
         <Container fluid className="px-0 main">
           <Navbar pageInfo={pageInfo} />
           <Row noGutters>
